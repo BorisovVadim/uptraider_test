ce5d3f6c389bc3a2d213afad74bc35bf01b3a4ff
vadimborisov <vadimborisovishere@gmail.com>
2023-04-06 11:16:10 +0300

    Сделал модели меню, реализовал возможность редактирования категорий и подменю в админке

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, TreeMenu
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Класс для отображения указанных полей
+     ModelAdmin в административной панели
+     """
+
+    fields = ['name']
+    list_display = ['__str__']
+
+
+@admin.register(TreeMenu)
+class TreeMenuAdmin(admin.ModelAdmin):
+    """Класс для отображения указанных полей
+    TreeMenu в административной панели
+    """
+
+    fields = ['name', 'category', 'path', 'parent']
+    list_display = ['__str__', 'category', 'path', 'parent']
--- app/models.py
+++ app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель для дерева категорий"""
+
+    class Meta:
+        """Класс Meta для назначения отображения
+        названий полей в административной панели
+        """
+        verbose_name = 'Menu category'
+        verbose_name_plural = 'Menu categories'
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        """Метод для отображения созданного имени
+        категории меню в административной панели
+        """
+        return self.name
+
+
+class TreeMenu(models.Model):
+    """Класс для дерева меню"""
+
+    class Meta:
+        verbose_name = 'Menu item'
+        verbose_name_plural = 'Menu items'
+
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        Category,
+        verbose_name='category',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False
+    )
+    path = models.CharField(verbose_name='path', max_length=255)
+
+    parent = models.ForeignKey(
+        'self',
+        verbose_name='parent',
+        on_delete=models.SET_DEFAULT,
+        null=True,
+        blank=True,
+        default=0
+    )
+
+    def __str__(self) -> str:
+        """Метод для отображения созданного имени
+        категории меню в административной панели
+        """
+        return self.name
